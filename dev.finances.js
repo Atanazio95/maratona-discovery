b5d093303176b67fd7e3fcaed92dda24930bb1dd
Gabriel <gabrielhorbachmachado@hotmail.com>
2021-02-01 19:33:54 -0300

    fix: erros no nome e no uso de algumas variaveis

--- dev.finances.js
+++ dev.finances.js
@@ -1,107 +1,107 @@
 const Modal = {
-    open() {
-        document.querySelector('.modal-overlay').classList.add('active')
-  
-    },
-    close() {
-        document.querySelector('.modal-overlay').classList.remove('active')
-    }
-     }  
-     
-     
-     const transactions = [{
-         id: 1,
-         description: 'Luz',
-         amout: -50000,
-         date: '23/01/2021'
-     },
-     {
-         id: 2,
-         description: 'Website',
-         amout: 500000,
-         date: '23/01/2021'
-     },
-     {
-         id: 3,
-         description: 'Internet',
-         amout: -20000,
-         date: '23/01/2021'
-     },
+  open() {
+    document.querySelector('.modal-overlay').classList.add('active')
 
-     {
-        id: 4,
-        description: 'App',
-        amout: 200000,
-        date: '23/01/2021'
-
-     }
-    
-    ]
-
-     const Transaction = {
-         // Somar as entradas
-         incomes() {
-             
-         },
-         // Somar as saidas
-         expenses() {
-
-         },
-         // Entradas - Saidas
-         total() {
-
-         }
-     }
-
-     const DOM = {
-         transactionsConteiner: document.querySelector('#data-table tbody'),
-
-         addTransaction(transaction, index){
-             const tr = document.createElement('tr')
-             tr.innerHTML = DOM.innerHTMLTransaction(transaction)
-
-             DOM.transactionsConteiner.appendChild(tr)
+  },
+  close() {
+    document.querySelector('.modal-overlay').classList.remove('active')
+  }
+}
 
 
-         },
-         innerHTMLTransaction() {
-             const CSSclass = transactions.amount > 0 ? "income" : "expense"
+const transactions = [{
+  id: 1,
+  description: 'Luz',
+  amount: -50000,
+  date: '23/01/2021'
+},
+{
+  id: 2,
+  description: 'Website',
+  amount: 500000,
+  date: '23/01/2021'
+},
+{
+  id: 3,
+  description: 'Internet',
+  amount: -20000,
+  date: '23/01/2021'
+},
 
-             const amount = Ultils.formatCurrency(transactions.amount)
+{
+  id: 4,
+  description: 'App',
+  amount: 200000,
+  date: '23/01/2021'
 
-            const html = `
-            
-            <td class="description">${transactions.description}</td>
-            <td class="${CSSclass}">${transactions.amount}</td>
-            <td class="date">${transactions.date}</td>
-            <td><img src="./assets/minus.svg" alt="Remover Transações"></td>
+}
 
-        
-        `
+]
 
-        return html
-        }
-     }
+const Transaction = {
+  // Somar as entradas
+  incomes() {
 
-     const Ultils = {
-         formatCurrency(value) {
-             const signal = Number(value) < 0 ? "-" : ""
+  },
+  // Somar as saidas
+  expenses() {
 
-             value = String(value).replace(/\D/g, "")
+  },
+  // Entradas - Saidas
+  total() {
 
-             value = Number(value) / 100
+  }
+}
 
-             value = value.toLocaleString("pt-BR" {
-                 style: "currency",
-                 currency: "BRL"
-             })
+const DOM = {
+  transactionsConteiner: document.querySelector('#data-table tbody'),
 
-             return signal + value
-             
-         }
-     }
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+    DOM.transactionsConteiner.appendChild(tr)
 
 
-     transaction.forEach(function(transaction) {
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
-     })
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td><img src="./assets/minus.svg" alt="Remover Transações"></td>
+
+
+    `
+
+    return html
+  }
+}
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "-" : ""
+
+    value = String(value).replace(/\D/g, "")
+
+    value = Number(value) / 100
+
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return signal + value
+
+  }
+}
+
+
+transactions.forEach(function (transaction) {
+  DOM.addTransaction(transaction)
+})
